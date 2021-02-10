e8f21e0e9f4b4cfe12ba6d1e9d17770126d9082f
Daniel Aros <danielaros200@gmail.com>
2021-02-10 16:33:11 -0600

    Falta agregar algunas excepciones

--- TorreDeHanoi.cpp
+++ TorreDeHanoi.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
 
 //Declaración de los métodos
 void comenzar(int[], int[], int[]);
@@ -21,44 +23,72 @@ int numDiscos, indiceCimaA, indiceCimaB, indiceCimaC;
 
 int main(){
 	int i = 0;
+	//Reglas del juego
+	cout<<"REGLAS DEL JUEGO:\n"<<"1.Puedes mover solamente un disco a la vez. \n2.Ningun disco puede estar encima de un disco mas pequeno."<<endl;
 	cout<<"Ingrese el numero de discos con los que se jugara: ";
 	cin>>numDiscos;
+	
+	//Creación de las pilas. Representan las varillas A,B,C.
 	int pilaA[numDiscos];
 	int pilaB[numDiscos];
 	int pilaC[numDiscos];
 	
-	comenzar(pilaA, pilaB, pilaC);
-	mostrarPila(pilaA, pilaB, pilaC);
+	comenzar(pilaA, pilaB, pilaC);//En este método se le asignan los valores a las pilas. 
+	mostrarPila(pilaA, pilaB, pilaC);//Método que muestra en consola las pilas.
 	
-	do{
+	int movimientosPer = pow(2, numDiscos)-1; //Se calculan los movimientos con la formula 2^n-1 (n es el numero de discos).
+	
+	do{ 
 		menu(pilaA, pilaB, pilaC);
 		i++;
-	}while(i < 7);
+	}while(i < movimientosPer);
 	
 	return 0;
 }
 
+void comenzar(int pilaA[], int pilaB[], int pilaC[]){
+	
+	int num = numDiscos; //Esta varibale sirve como auxiliar para rellenar la pila A.
+	
+	for(int i = 0; i < numDiscos; i++){
+		pilaA[i] = num;//En la posicion cero de la pilaA que es la inicial se tendra el numero mayor de disco
+		num = num - 1;
+		pilaB[i] = 0;//La pila A y B se llenan con un valor cero ya que estaran vacias.
+		pilaC[i] = 0;
+	}
+	
+	//Se inician los indices de la cima de cada pila, las pilas A,B tendran indices 
+	indiceCimaA = numDiscos;
+	indiceCimaB = 0;
+	indiceCimaC = 0;
+}
+
 void menu(int pilaA[], int pilaB[], int pilaC[]){
 	int opcionDesapilar, opcionApilar;
-	int e = 0;
+	int e;
+	
 	cout<<"Escoja la varilla a la que se le desapilara un disco: \n 1. A \n 2. B \n 3. C"<<endl;
 	cin>>opcionDesapilar;
 	cout<<"Escoja la varilla a la que se apilara un disco: \n 1. A \n 2. B \n 3. C"<<endl;
 	cin>>opcionApilar;
 	
-	switch(opcionDesapilar){
+	switch(opcionDesapilar){//De acuerdo a la opcion elegida para desapilar, se recoge el elemento que esta en la cima y posteriormente se desapila el elemento. 
 		case 1:
 			e = consultarCimaA(pilaA);
+			desapilarA(pilaA);
 			break;
 		case 2:
 			e = consultarCimaB(pilaB);
+			desapilarB(pilaB);
 			break;
 		case 3: 
 			e = consultarCimaC(pilaC);
+			desapilarC(pilaC);
 			break;
 	}
 	
-	switch(opcionApilar){
+	//De acuerdo a la opcion elegida para apilar, se pasara el elemento que se recogio de desapilar y se le pasa como paramentro junto a la pila correspondiente.
+	switch(opcionApilar){ 
 		case 1:
 			apilarA(e, pilaA);
 			break;
@@ -69,60 +99,23 @@ void menu(int pilaA[], int pilaB[], int pilaC[]){
 			apilarC(e, pilaC);
 			break;
 	}
-	
-	switch(opcionDesapilar){
-		case 1:
-			
-			desapilarA(pilaA);
-			break;
-		case 2:
-			desapilarB(pilaB);
-			break;
-		case 3: 
-			desapilarC(pilaC);
-			break;
-	}
-	
-	mostrarPila(pilaA, pilaB, pilaC);
-	
-}
-
-void comenzar(int pilaA[], int pilaB[], int pilaC[]){
-	
-	int num = numDiscos; //Esta varibale sirve como auxiliar para rellenar la pila A.
-	
-	for(int i = 0; i < numDiscos; i++){
-		pilaA[i] = num;
-		num = num - 1;
-		pilaB[i] = numDiscos+1;
-		pilaC[i] = numDiscos+1;
-	}
-	
-	indiceCimaA = numDiscos;
-	indiceCimaB = 0;
-	indiceCimaC = 0;
+		
+	mostrarPila(pilaA, pilaB, pilaC);//Se muestra las pilas ya modificadas.
 }
 
 void mostrarPila(int pilaA[], int pilaB[], int pilaC[]){
-	for(int i = 0; i < indiceCimaA; i++){
-		cout<<pilaA[i]<<" ";
+	cout<<"A"<<" B"<<" C"<<endl;
+	
+	int aux = numDiscos-1;//Esta variable es auxiliar para mostrar ascendentemente los valores que almacena cada posicion de la pila.
+	
+	for(int i = 0; i < numDiscos; i++){
+		cout<<pilaA[aux]<<" "<<pilaB[aux]<<" "<<pilaC[aux]<<endl;
+		aux = aux-1;
 	}
-	
-	cout<<endl;
-	
-	for(int i = 0; i < indiceCimaB; i++){
-		cout<<pilaB[i]<<" ";
-	}
-	
-	cout<<endl;
-	
-	for(int i = 0; i < indiceCimaC; i++){
-		cout<<pilaC[i]<<" ";
-	}
-	
-	cout<<"Cima A: "<<indiceCimaA<<endl;
+
+	/*cout<<"\nCima A: "<<indiceCimaA<<endl;
 	cout<<"Cima B: "<<indiceCimaB<<endl;
-	cout<<"Cima C: "<<indiceCimaC<<endl;
+	cout<<"Cima C: "<<indiceCimaC<<endl;*/
 }
 
 void apilarA(int e, int p[]){
@@ -147,7 +140,8 @@ void apilarB(int e, int p[]){
 		}else{
 			p[indiceCimaB] = e;
 			indiceCimaB = indiceCimaB+1;
-			/*if(p[indiceCimaB] < e){
+			/*
+			if(p[indiceCimaB] < e){
 				cout<<"El movimiento es incorrecto"<<endl;
 			}else{
 				p[indiceCimaB] = e;
@@ -176,6 +170,7 @@ void desapilarA(int p[]){
 	if(indiceCimaA == 0){
 		cout<<"Pila vacia"<<endl;
 	}else{
+		p[indiceCimaA-1] = 0;
 		indiceCimaA = indiceCimaA-1;
 	}
 }
@@ -184,6 +179,7 @@ void desapilarB(int p[]){
 	if(indiceCimaB == 0){
 		cout<<"Pila vacia"<<endl;
 	}else{
+		p[indiceCimaB-1] = 0;
 		indiceCimaB = indiceCimaB-1;
 	}
 }
@@ -192,6 +188,7 @@ void desapilarC(int p[]){
 	if(indiceCimaC == 0){
 		cout<<"Pila vacia"<<endl;
 	}else{
+		p[indiceCimaC-1] = 0;
 		indiceCimaC = indiceCimaC-1;
 	}
 }
@@ -204,7 +201,6 @@ int consultarCimaA(int p[]){
 		cout<<"indice cima A: "<<indiceCimaA<<endl;
 		e = p[indiceCimaA-1];
 	}
-	
 	return e;
 }
 
@@ -214,8 +210,7 @@ int consultarCimaB(int p[]){
 		cout<<"Pila vacia"<<endl;
 	}else{
 		e = p[indiceCimaB-1];
-	}
-	
+	}	
 	return e;
 }
 
@@ -226,6 +221,5 @@ int consultarCimaC(int p[]){
 	}else{
 		e = p[indiceCimaC-1];
 	}
-	
 	return e;
 }
